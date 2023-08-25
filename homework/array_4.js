55aee5f22dc70e355991bd4d41f8e7799ee7b3e5
Edward <torresthebes@mail.ru>
2023-08-25 11:07:59 +0300

    new commint

--- homework/array_4.js
+++ homework/array_4.js
@@ -9,3 +9,5 @@ const sumRows = console.log(array.map((n) => n.reduce((sum, m) => sum + m, 0)));
 const sumCols = console.log(
   array[0].map((n, i) => array.reduce((sum, m) => sum + m[i], 0))
 );
+
+//new
