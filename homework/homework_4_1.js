e38e2304a64b9f1d6aa5da0ff2ec8b8a81550efb
Edward <torresthebes@mail.ru>
2023-09-07 17:42:04 +0300

    Comment

--- homework/homework_4_1.js
+++ homework/homework_4_1.js
@@ -1,7 +1,7 @@
 const arrrr = [1, -2, 3, -4, 5, -6, 7, -8];
 
-function revomeNegative(values){
-return values >= 0;
+function revomeNegative(values) {
+  return values >= 0;
 }
 
 let result = arrrr.filter(revomeNegative);
--- homework/homework_4_2.js
+++ homework/homework_4_2.js
@@ -1,9 +1,9 @@
-const input = 'I love Intrahealth';
+const input = "I love Intrahealth";
 
-function revomeSpace(text){
-    return text.replaceAll(' ', '');
-    }
-    
-    let result = revomeSpace(input);
-    
-    console.log(result);
+function revomeSpace(text) {
+  return text.replaceAll(" ", "");
+}
+
+let result = revomeSpace(input);
+
+console.log(result);
